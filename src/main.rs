a376f55c2a61fdaad8df0b618a0f1d039843c68e
Michal Budzynski <budziq@gmail.com>
2017-06-21 07:34:54 +0200

    the reproduction is not yet minimal

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+
+#[cfg(windows)]
+mod execs {
+    pub const NPM: &'static str = "npm.cmd";
+}
+#[cfg(not(windows))]
+mod execs {
+    pub const NPM: &'static str = "npm";
+}
+
+fn main() {
+    let status = Command::new(execs::NPM)
+        .args(&["list", "-g"])
+        .arg("stylus")
+        .output();
+
+    println!("Command exited with '{:?}'", status);
+    let status = status.expect("Process spawn should succeed!");
+    if !status.status.success() {
+        panic!("Command failed!");
+    }
+}
